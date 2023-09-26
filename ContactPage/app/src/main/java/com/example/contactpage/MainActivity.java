97ee2ffce32b80a66609bf77422ede4913ef6ab7
Kunal Bindra <111312262+KunalBindra@users.noreply.github.com>
2023-09-26 19:29:18 +0530

    Add files via upload

---
+++ ContactPage/app/src/main/java/com/example/contactpage/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.contactpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        private Button button3;
+        button.findViewById(R.id.button3);
+        button.setOnClickListener();
+    }
+}
